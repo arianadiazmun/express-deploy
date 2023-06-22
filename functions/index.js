2cd93320e7c6732b1188eb482c65a7ad2cda2080
Ariana Diaz <arianadiaz@Arianas-MacBook-Air.local>
2023-06-22 16:14:44 -0400

    added update

--- functions/index.js
+++ functions/index.js
@@ -4,20 +4,20 @@ import {onRequest} from "firebase-functions/v2/https";
 import logger from "firebase-functions/logger";
 import express from "express";
 import cors from "cors";
-import { getAllCandy,addNewCandy } from "./src/candy.js";
+import { getAllCandy,addNewCandy ,updateCandyById} from "./src/candy.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/test", (req,res)=> {
+app.get("/test", (req, res)=> {
  logger.info("Someone hit my API");
     res.send("It's working!😃")
 });
 
 app.get("/candy", getAllCandy);
-
 app.post("/candy",addNewCandy);
+app.patch("/candy/:candyId", updateCandyById);
 
 export const api = onRequest(app);
 
--- functions/src/candy.js
+++ functions/src/candy.js
@@ -12,6 +12,7 @@ try{
 }
 
  }
+
 export async function addNewCandy (req,res) {
     try {
         const newCandy = req.body;
@@ -21,3 +22,16 @@ export async function addNewCandy (req,res) {
         res.status(500).send(err);
     }
 }
+
+
+export async function updateCandyById(req,res) {
+    try {
+        //const candyId = req.params.candyId
+        const { candyId }= req.params; //which candy are we changing
+        const updatedInfo= req.body;//what changes are we making (e.x {price 2.99})
+        await coll.doc(candyId).update(updatedInfo);//update the doc with the new info
+           getAllCandy(req, res);
+     } catch(err) {
+        res.status(500).send(err)
+        }
+    }
